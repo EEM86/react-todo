4b03c61660eef30b9b4d32de688c73156750f86a
EEM <eevgeniy@gmail.com>
2020-03-15 22:01:40 +0200

    init todo app

---
+++ src/components/app-header/app-header.js
@@ -0,0 +1,12 @@
+import React from "react"
+import './app-header.css'
+
+const AppHeader = () => {
+    return (
+        <span className="app-header">
+            <h1>My Todo List</h1>
+        </span>
+    );
+};
+
+export default AppHeader;
